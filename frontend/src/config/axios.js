add90e365cb5efa46717da0945447aaa7576a663
rushikesh <danve2090@gmail.com>
2024-09-08 22:11:34 +0530

    url

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,3 +1,3 @@
 import axios from 'axios'
 
-export const axiosi=axios.create({withCredentials:true,baseURL:process.env.REACT_APP_BASE_URL})
+export const axiosi=axios.create({withCredentials:true,baseURL:"https://e-commerce-beige-gamma-37.vercel.app"})
